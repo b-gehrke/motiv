723bb2edaf8d0ee216a0cedad4c02c7bdc167156
Florian Gallrein <florian@gallrein.de>
2022-11-27 13:49:05 +0100

    Add filter and search menu entry

--- src/ui/windows/mainwindow.cpp
+++ src/ui/windows/mainwindow.cpp
@@ -21,36 +21,54 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), dockWidgets() {
 }
 
 void MainWindow::createMenus() {
-  /// File menu
-  // "Open Trace" menu entry
-  auto openTraceAction = new QAction(tr("&Open Trace"), this);
-  openTraceAction->setShortcut(tr("Ctrl+O"));
-  connect(openTraceAction, SIGNAL(triggered()), this, SLOT(openTrace()));
+    /// File menu
+    // "Open Trace" menu entry
+    auto openTraceAction = new QAction(tr("&Open Trace"), this);
+    openTraceAction->setShortcut(tr("Ctrl+O"));
+    connect(openTraceAction, SIGNAL(triggered()), this, SLOT(openTrace()));
 
-  // "Close Trace" menu entry
-  auto closeTraceAction = new QAction(tr("&Close Trace"), this);
-  closeTraceAction->setShortcut(tr("Ctrl+W"));
-  connect(closeTraceAction, SIGNAL(triggered()), this, SLOT(closeTrace()));
+    // "Close Trace" menu entry
+    auto closeTraceAction = new QAction(tr("&Close Trace"), this);
+    closeTraceAction->setShortcut(tr("Ctrl+W"));
+    connect(closeTraceAction, SIGNAL(triggered()), this, SLOT(closeTrace()));
 
-  // "Quit" menu entry
-  auto quitAction = new QAction(tr("&Quit"), this);
-  quitAction->setShortcut(tr("Ctrl+Q"));
-  connect(quitAction, SIGNAL(triggered()), this, SLOT(close()));
+    // "Quit" menu entry
+    auto quitAction = new QAction(tr("&Quit"), this);
+    quitAction->setShortcut(tr("Ctrl+Q"));
+    connect(quitAction, SIGNAL(triggered()), this, SLOT(close()));
 
-  auto fileMenu = menuBar()->addMenu(tr("&File"));
-  fileMenu->addAction(openTraceAction);
-  fileMenu->addSeparator();
-  fileMenu->addAction(closeTraceAction);
-  fileMenu->addAction(quitAction);
+    auto fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(openTraceAction);
+    fileMenu->addSeparator();
+    fileMenu->addAction(closeTraceAction);
+    fileMenu->addAction(quitAction);
 
-  /// Help menu
-  // "View license" menu entry
-  auto aboutAction = new QAction(tr("&View license"), this);
-  // TODO connect to actual action
-  connect(aboutAction, SIGNAL(triggered()), this, SLOT(close()));
+    /// View Menu
+    // "Filter" menu entry
+    auto filterAction = new QAction(tr("&Filter"));
+    // TODO S for sieve? what might be more intuitive?
+    filterAction->setShortcut(tr("Ctrl+S"));
+    // TODO add actual slot
+    connect(filterAction, SIGNAL(triggered()), this, SLOT(openFilterPopup()));
 
-  auto helpMenu = menuBar()->addMenu(tr("&Help"));
-  helpMenu->addAction(aboutAction);
+    // "Search" menu entry
+    auto searchAction = new QAction(tr("&Search"));
+    searchAction->setShortcut(tr("Ctrl+F"));
+    // TODO add actual slot
+    connect(searchAction, SIGNAL(triggered()), this, SLOT(openFilterPopup()));
+
+    auto viewMenu = menuBar()->addMenu(tr("&View"));
+    viewMenu->addAction(filterAction);
+    viewMenu->addAction(searchAction);
+
+    /// Help menu
+    // "View license" menu entry
+    auto aboutAction = new QAction(tr("&View license"), this);
+    // TODO connect to actual action
+    connect(aboutAction, SIGNAL(triggered()), this, SLOT(close()));
+
+    auto helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(aboutAction);
 }
 
 void MainWindow::createToolBars() {
