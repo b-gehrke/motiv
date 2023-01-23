f213d01a7ac610de0ce082f206db087c820296ad
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2023-01-23 21:55:51 +0100

    Added communication visualization

--- src/ui/TimelineView.cpp
+++ src/ui/TimelineView.cpp
@@ -1,4 +1,6 @@
 #include "TimelineView.hpp"
+#include "CommunicationIndicator.hpp"
+#include "SlotIndicator.hpp"
 
 #include <QGraphicsRectItem>
 
@@ -10,23 +12,27 @@ TimelineView::TimelineView(TraceDataProxy *data, QWidget *parent) : QGraphicsVie
     this->setScene(scene);
     this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
-    populateScene(scene);
-
     connect(this->data, SIGNAL(selectionChanged()), this, SLOT(updateView()));
 }
+
 
 void TimelineView::populateScene(QGraphicsScene *scene) {
     auto width = scene->width();
     auto selection = this->data->getSelection();
     auto runtime = selection->getRuntime().count();
+    auto runtimeR = static_cast<qreal>(runtime);
     auto begin = this->data->getBegin().count();
+    auto beginR = static_cast<qreal>(begin);
     auto end = begin + runtime;
+    auto endR = static_cast<qreal>(end);
+
+    QPen arrowPen(Qt::black, 1);
 
     auto top = 0;
     auto ROW_HEIGHT = 30;
-    for (const auto& item : selection->getSlots()) {
+    for (const auto &item: selection->getSlots()) {
         // Display slots
-        for (const auto& slot : item.second) {
+        for (const auto &slot: item.second) {
             auto region = slot->region;
             auto regionName = region->name();
             auto regionNameStr = regionName.str();
@@ -39,20 +45,22 @@ void TimelineView::populateScene(QGraphicsScene *scene) {
             // Ensures slots ending after `end` (like main) are considered to end at end
             auto effectiveEndTime = qMin(end, endTime);
 
-            auto slotBeginPos = qMax(0.0, (static_cast<qreal>(effectiveStartTime - begin) / static_cast<qreal>(runtime)) * width);
+            auto slotBeginPos = qMax(0.0,
+                                     (static_cast<qreal>(effectiveStartTime - begin) / static_cast<qreal>(runtime)) *
+                                     width);
             auto slotRuntime = static_cast<qreal>(effectiveEndTime - effectiveStartTime);
             auto rectWidth = (slotRuntime / static_cast<qreal>(runtime)) * width;
 
             QRectF rect(slotBeginPos, top, qMax(rectWidth, 5.0), ROW_HEIGHT);
-            auto rectItem = scene->addRect(rect);
+            auto rectItem = new SlotIndicator(rect);
             rectItem->setToolTip(regionNameStr.c_str());
 
             // Determine color based on name
             QColor rectColor;
-            if(regionNameStr.starts_with("MPI_")) {
+            if (regionNameStr.starts_with("MPI_")) {
                 rectColor = Qt::green;
                 rectItem->setZValue(3);
-            } else if(regionNameStr.starts_with("!$omp")) {
+            } else if (regionNameStr.starts_with("!$omp")) {
                 rectColor = Qt::red;
                 rectItem->setZValue(2);
             } else {
@@ -60,11 +68,46 @@ void TimelineView::populateScene(QGraphicsScene *scene) {
                 rectItem->setZValue(1);
             }
             rectItem->setBrush(rectColor);
+            scene->addItem(rectItem);
         }
 
         top += ROW_HEIGHT;
     }
+
+    for (const auto &communication: selection->getCommunications()) {
+        const CommunicationEvent *startEvent = communication->getStart();
+        auto startEventEnd = static_cast<qreal>(startEvent->getEnd().count());
+        auto startEventStart = static_cast<qreal>(startEvent->getStart().count());
+
+
+        const CommunicationEvent *endEvent = communication->getEnd();
+        auto endEventEnd = static_cast<qreal>(startEvent->getEnd().count());
+        auto endEventStart = static_cast<qreal>(startEvent->getStart().count());
+
+
+        auto fromTime = startEventStart + (startEventEnd - startEventStart) / 2;
+        auto effectiveFromTime = qMax(beginR, fromTime) - beginR;
+
+        auto toTime = endEventStart + (endEventEnd - endEventStart) / 2;
+        auto effectiveToTime = qMin(endR, toTime) - beginR;
+
+        auto fromRank = startEvent->getLocation()->ref().get();
+        auto toRank = endEvent->getLocation()->ref().get();
+
+        auto fromX = effectiveFromTime / runtimeR * width;
+        auto fromY = static_cast<qreal>(fromRank * ROW_HEIGHT) + .5 * ROW_HEIGHT;
+
+        auto toX = effectiveToTime / runtimeR * width;
+        auto toY = static_cast<qreal> (toRank * ROW_HEIGHT) + .5 * ROW_HEIGHT;
+
+        auto arrow = new CommunicationIndicator(fromX, fromY, toX, toY);
+        arrow->setPen(arrowPen);
+        arrow->setZValue(5);
+        scene->addItem(arrow);
+    }
+
 }
+
 
 void TimelineView::resizeEvent(QResizeEvent *event) {
     this->updateView();
@@ -73,7 +116,7 @@ void TimelineView::resizeEvent(QResizeEvent *event) {
 
 void TimelineView::updateView() {
     // TODO it might be more performant to keep track of items and add/remove new/leaving items and resizing them
-    delete this->scene();
+//    delete this->scene();
     auto scene = new QGraphicsScene(this);
 
     auto ROW_HEIGHT = 30;
